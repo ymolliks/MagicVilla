7ccbe286a4e5177432b8861a01eb0a9ae160425b
Yeasin Mollik <yeasin1603@cseku.ac.bd>
2024-03-20 19:55:13 +0000

    Implement endpoint for deleting villa

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -67,5 +67,26 @@ namespace MagicVilla_VillaAPI.Controllers
             
             return CreatedAtRoute("GetVilla", new { id = villa.Id }, villa);
         }
+
+        [HttpDelete("{id:int}", Name = "DeleteVilla")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult DeleteVilla(int id)
+        {
+            if(id == 0)
+            {
+                return BadRequest();
+            }
+
+            var villa = VillaStore.VillaList.FirstOrDefault(v => v.Id == id);
+            if(villa == null)
+            {
+                return NotFound();
+            }
+
+            VillaStore.VillaList.Remove(villa);            
+            return NoContent();
+        }
     }
 }
